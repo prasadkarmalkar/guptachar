a6daa91bdb39ac916dab5dd61d1482684ccf339f
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2024-08-18 16:03:01 +0530

    Formatting

--- src/components/ShareForm.tsx
+++ src/components/ShareForm.tsx
@@ -20,28 +20,36 @@ function ShareForm() {
 			</div>
 			<div className='flex flex-col sm:flex-row items-center mt-6 border-gray-700 border p-4 rounded-md gap-2'>
 				<div className='flex flex-1 items-center justify-between w-full'>
-                <label htmlFor='expiry'>Expire after</label>
-				<select
-					name='expiry'
-					id='expiry'
-					className='cursor-pointer bg-black border-gray-700 border rounded-md px-1 pr-10 py-1 '
-				>
-					<option value='1'>1 day</option>
-					<option value='3'>3 days</option>
-					<option value='7'>1 week</option>
-				</select>
-                </div>
-                <div className='flex flex-1 items-center justify-between w-full'>
-				<label htmlFor='open-expire'>Expire on open</label>
-				<label className='inline-flex items-center cursor-pointer'>
-					<input type='checkbox' name='open-expire' id='expire-open' value='' className='sr-only peer' />
-					<div className="relative w-11 h-6 bg-gray-200 border-gray-700 border peer-focus:outline-none rounded-full peer dark:bg-black peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-700"></div>
-				</label>
-                </div>
-
-
+					<label htmlFor='expiry'>Expire after</label>
+					<select
+						name='expiry'
+						id='expiry'
+						className='cursor-pointer bg-black border-gray-700 border rounded-md px-1 pr-10 py-1 '
+					>
+						<option value='1'>1 day</option>
+						<option value='3'>3 days</option>
+						<option value='7'>1 week</option>
+					</select>
+				</div>
+				<div className='flex flex-1 items-center justify-between w-full'>
+					<label htmlFor='open-expire'>Expire on open</label>
+					<label className='inline-flex items-center cursor-pointer'>
+						<input
+							type='checkbox'
+							name='open-expire'
+							id='expire-open'
+							value=''
+							className='sr-only peer'
+						/>
+						<div className="relative w-11 h-6 bg-gray-200 border-gray-700 border peer-focus:outline-none rounded-full peer dark:bg-black peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-700"></div>
+					</label>
+				</div>
 			</div>
-			<input type="submit" value="Generate Link" className='mt-10 rounded-md border bg-gray-900 border-gray-700 px-5 py-2 hover:cursor-pointer hover:bg-black' />
+			<input
+				type='submit'
+				value='Generate Link'
+				className='mt-10 rounded-md border bg-gray-900 border-gray-700 px-5 py-2 hover:cursor-pointer hover:bg-black'
+			/>
 		</form>
 	);
 }
